8efcbc19198c6b9ff2d1ce42af7018f38c5dab1d
cmm1 <qq1036641205@163.com>
2023-02-25 15:07:44 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import store from './store'
+import store from './stores'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
